f4fb13dc4a17732d245f936b34bb259bec935a69
Ekberjan <ekberjanderman@gmail.com>
2016-06-08 12:32:05 +0300

    Update main.cpp

--- myIris/main.cpp
+++ myIris/main.cpp
@@ -6,16 +6,16 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::string path = "D:/Workspace/C++/Iris/IrisRecognition/x64/Release/Images/";
-	cv::Mat genImage = cv::imread(path + "029/R/1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	cv::Mat testImage = cv::imread(path + "029/R/2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	std::string path = "/Images/";
+	cv::Mat genImage = cv::imread(path + "/1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	cv::Mat testImage = cv::imread(path + "/2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
 	try
 	{
 		Proline::pirManager pir;
 		pir.loadConfiguration("D:/Workspace/C++/Iris/myIris/x64/Release/configuration.ini");		
 		pir.run(genImage, testImage, "1.jpg", "2.jpg");
-		std::cout << "score: " << pir.getScore() << std::endl;
+		std::cout << "matching score: " << pir.getScore() << std::endl;
 
 	}
 	catch (std::exception e)
